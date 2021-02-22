16db32223250693886f813f477e8facb444dea21
jefferson.souza <jefferson.souza@synchro.com.br>
2021-02-22 10:19:25 -0300

    sum values from file

--- lib/wonderpay/numbers.ex
+++ lib/wonderpay/numbers.ex
@@ -1,10 +1,17 @@
 defmodule Wonderpay.Numbers do
   def sum_from_file(filename) do
-    file = File.read("#{filename}.csv")
-    handle_file(file)
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
   end
 
-  defp handle_file({:ok, file}), do: file
+  defp handle_file({:ok, result}) do
+    result = String.split(result, ",")
+    result = Enum.map(result, fn number -> String.to_integer(number) end)
+    result = Enum.sum(result)
+    result
+  end
+
   defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
 
 end
