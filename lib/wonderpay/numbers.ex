dc70f77ede9a0d1c923b86add3b11bd98892d457
jefferson.souza <jefferson.souza@synchro.com.br>
2021-02-22 10:13:38 -0300

    reading data from csv

---
+++ lib/wonderpay/numbers.ex
@@ -0,0 +1,10 @@
+defmodule Wonderpay.Numbers do
+  def sum_from_file(filename) do
+    file = File.read("#{filename}.csv")
+    handle_file(file)
+  end
+
+  defp handle_file({:ok, file}), do: file
+  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+
+end
