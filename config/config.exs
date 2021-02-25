c65d69287ee5ad1227adb8a8473d18a4b1b7874a
Jefferson M Souza <jefferson.msouza86@gmail.com>
2021-02-24 22:42:02 -0300

    setting up schema for users

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,10 @@ config :wonderpay, WonderpayWeb.Endpoint,
   pubsub_server: Wonderpay.PubSub,
   live_view: [signing_salt: "JqIyXSbs"]
 
+config :wonderpay, Wonderpay.Repo,
+  migration_primary_key: [type: :binary_id],
+  migration_foreign_key: [type: :binary_id]
+
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
